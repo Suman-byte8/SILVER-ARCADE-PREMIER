b84b56fe96a3d2a873555b2c72e41a9afd324b0d
SumanSaha <sumansahaweb.dev@gmail.com>
2025-08-27 15:15:39 +0530

    fixed distinctive layout

--- Client/src/components/Home Page/Distinctive/Distinctive.jsx
+++ Client/src/components/Home Page/Distinctive/Distinctive.jsx
@@ -42,7 +42,7 @@ const Distinctive = () => {
   } : null;
 
   return (
-    <section className="w-full py-12 flex flex-col items-center gap-8 px-4 sm:px-6 lg:px-8 relative z-10">
+    <section className="w-full py-12 mt-18 md:mt-2 flex flex-col items-center gap-8 px-4 sm:px-6 lg:px-8 relative z-10">
       <FullLogo isFlex />
 
       <div className="w-full sm:w-[90%] md:w-[70%] text-center">
@@ -58,11 +58,11 @@ const Distinctive = () => {
       </div>
 
       {/* Links (horiz scroll if overflow) */}
-      <div className="overflow-x-auto flex items-center justify-start sm:justify-center gap-3 sm:gap-4 md:gap-6 pb-2">
+      <div className="overflow-x-auto flex flex-wrap  items-center justify-center sm:justify-center gap-3 sm:gap-4 md:gap-6 pb-2">
         {distinctives.map((d) => (
           <button
             key={d._id}
-            className={`whitespace-nowrap px-2 py-1 text-xs sm:text-sm transition-all duration-300 border-b-2 ${
+            className={`whitespace-nowrap px-2 py-1 text-sm transition-all duration-300 border-b-2 ${
               selectedLink?._id === d._id
                 ? "border-gray-700 font-semibold text-gray-800"
                 : "border-transparent hover:border-gray-300 text-gray-600"
